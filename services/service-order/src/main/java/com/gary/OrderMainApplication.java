ee1f8e077511c6f46819a4267fa3019423d9f539
gary.chen <ch66971542@gmail.com>
2025-07-06 20:58:11 +0800

    feat: 練習Nacos配置中心基本用法-3
    - 直接在啟動類內配置任務監聽

--- services/service-order/src/main/java/com/gary/OrderMainApplication.java
+++ services/service-order/src/main/java/com/gary/OrderMainApplication.java
@@ -1,7 +1,15 @@
 package com.gary;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class OrderMainApplication {
@@ -10,4 +18,28 @@ public class OrderMainApplication {
         SpringApplication.run(OrderMainApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner run(NacosConfigManager nacosConfigManager) {
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener(
+                    "service-order.properties",
+                    "DEFAULT_GROUP",
+                    new Listener() {
+
+                        @Override
+                        public Executor getExecutor() { // 線程池
+                            return Executors.newFixedThreadPool(4);
+                        }
+
+                        @Override
+                        public void receiveConfigInfo(String s) {
+                            System.out.println("變化的配置信息:" + s);
+                            System.out.println("寄送Email");
+                        }
+                    }
+            );
+            System.out.println("執行Nacos配置監聽");
+        };
+    }
 }
