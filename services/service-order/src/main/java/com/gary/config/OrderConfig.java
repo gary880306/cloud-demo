ff5c8512b43f4397c7b5c147d2c021440666143d
gary.chen <ch66971542@gmail.com>
2025-07-06 23:16:29 +0800

    feat: 添加OpenFeign日誌
    - yml文件配置 & config配置@Bean

--- services/service-order/src/main/java/com/gary/config/OrderConfig.java
+++ services/service-order/src/main/java/com/gary/config/OrderConfig.java
@@ -1,5 +1,6 @@
 package com.gary.config;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,11 @@ public class OrderConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
